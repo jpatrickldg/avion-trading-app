26c5a78e4f0f9b705398ec30fb205ee57151beaa
jpatrickldg <jpatrickldg@gmail.com>
2022-09-03 20:36:52 +0800

    Add home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  devise_for :users
+  # get '/users/:user_id' => "pages#dashboard", :as => :user_root
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

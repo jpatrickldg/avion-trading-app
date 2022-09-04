452f4ae095ca13991b2d5586136a4120cdfbabe6
jpatrickldg <jpatrickldg@gmail.com>
2022-09-04 15:36:26 +0800

    Add Portfolio

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,30 @@
 class PagesController < ApplicationController
   before_action :get_user
+  before_action :authenticate_user!, except: [:home]
+  before_action :set_stocks_and_transactions, only: [:portfolio, :transactions]
 
   def home
     @users = User.all
   end
 
   def portfolio
+    require 'iex-ruby-client'
+    @unique = @transactions.select('stock_id as stock_id, sum(quantity) as total_quantity').group(:stock_id)
+
+    @client = IEX::Api::Client.new(
+      publishable_token: 'pk_b51bcad458d549c68be92ebc5cb43be4',
+      secret_token: 'sk_ec3e99fa21634d04aa7b8b7f70ad2a8c',
+      endpoint: 'https://cloud.iexapis.com/v1')
+    @client_list = @client.stock_market_list(:mostactive)
   end
 
   def transactions
-      @transactions = @user.transactions
-      @stock = Stock.all
+    
+  end
+
+  def set_stocks_and_transactions
+    @transactions = @user.transactions
+    @stocks = Stock.all
   end
 
   def get_user
--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,20 +1,26 @@
 class StocksController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_api, :get_user
 
   def index
     @stocks = Stock.all
   end
 
   def show
-    require 'iex-ruby-client'
     @stock = Stock.find(params[:id])
+    @quote = @client.quote(@stock.symbol)
+  end
 
+  def set_api
+    require 'iex-ruby-client'
     @client = IEX::Api::Client.new(
       publishable_token: 'pk_b51bcad458d549c68be92ebc5cb43be4',
       secret_token: 'sk_ec3e99fa21634d04aa7b8b7f70ad2a8c',
       endpoint: 'https://cloud.iexapis.com/v1')
     @client_list = @client.stock_market_list(:mostactive)
-    @quote = @client.quote(@stock.symbol)
+  end
 
+  def get_user
+    @user = current_user
   end
 end

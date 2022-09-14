0e1c0bd6d63ae84e55b4f86c977991060eeca192
jpatrickldg <jpatrickldg@gmail.com>
2022-09-14 17:54:00 +0800

    Change SMTP settings to Production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,17 +72,17 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # config.action_mailer.perform_deliveries = true
   # SMTP settings for mailcatcher
-  config.action_mailer.smtp_settings = {
-    address: '127.0.0.1',
-    port: 1025
-  }
-
   # config.action_mailer.smtp_settings = {
-  #   :address              => "smtp.gmail.com",
-  #   :port                 => 587,
-  #   :user_name            => ENV['GMAIL'],
-  #   :password             => ENV['GMAIL_PASS'],
-  #   :authentication       => "plain",
-  #   # :enable_starttls_auto => true
+  #   address: '127.0.0.1',
+  #   port: 1025
   # }
+
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['GMAIL'],
+    :password             => ENV['GMAIL_PASS'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 end

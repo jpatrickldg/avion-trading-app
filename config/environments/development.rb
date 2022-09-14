20be9ab3caa83234ed93221bd978524d3f175bef
jpatrickldg <jpatrickldg@gmail.com>
2022-09-14 16:15:56 +0800

    Add ENV

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -80,8 +80,8 @@ Rails.application.configure do
   # config.action_mailer.smtp_settings = {
   #   :address              => "smtp.gmail.com",
   #   :port                 => 587,
-  #   :user_name            => "avion.stock.app@gmail.com",
-  #   :password             => "vyotsllvjgbprsyz",
+  #   :user_name            => ENV['GMAIL'],
+  #   :password             => ENV['GMAIL_PASS'],
   #   :authentication       => "plain",
   #   # :enable_starttls_auto => true
   # }
